f4e8947eab1c6ffeb58ddf2318a7e853d6178b6e
mocao <mocaor2007@gmail.com>
2026-04-29 21:51:25 +0200

    vbuo[j,

---
+++ src/main/java/simulacroExamen/Libro.java
@@ -0,0 +1,14 @@
+package simulacroExamen;
+
+import java.util.regex.Pattern;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String descripcion;
+    private String isbn;
+
+
+
+
+}
